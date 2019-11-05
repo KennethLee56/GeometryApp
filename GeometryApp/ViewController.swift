566e4fc33a52527a911c204825ed9bd5e0751130
Kenneth Lee <kennethvlee@gmail.com>
2019-11-05 11:14:14 -0500

    Initial Commit

---
+++ GeometryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeometryApp
+//
+//  Created by Lee, Kenneth Van on 11/5/19.
+//  Copyright © 2019 Lee, Kenneth Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
